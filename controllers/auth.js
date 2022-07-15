c4cea58ffc6982b318f83b12a5dfa635022aba0f
maualice <alicemauroe@gmail.com>
2022-07-15 20:39:15 -0300

    generando token de usuario creado

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
   /* const { name, email, password } = req.body;
@@ -15,7 +15,10 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user });*/
   //todo esto se puede sacar al usar middleware pre de mongoose
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userId: user._id, name: user.name }, 'jwtSecret', {
+    expiresIn: '30d',
+  });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
